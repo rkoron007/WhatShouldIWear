655eb11be644a243e50f2e62b55e8785ca94536e
rkoron <rkoron007@gmail.com>
2020-01-21 12:58:37 -0800

    Add break to Greeting message line

--- client/components/App.js
+++ client/components/App.js
@@ -11,7 +11,7 @@ const Greeting = () => {
     <Jumbotron>
       <h1>What Should I Wear?</h1>
       <p>
-        Have you ever looked at the forecast and not known what to wear?
+        Have you ever looked at the forecast and not known what to wear?{" "}
         <strong>We are here to help!</strong>
         <br></br>
         Enter a city below for a 5+ day weather forecast and clothing
